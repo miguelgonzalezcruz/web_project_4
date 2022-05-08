846e74ff4b28b199bdb486c9b36c6be3f974400e
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2022-05-08 23:35:13 +0200

    Merge branch 'main' of https://github.com/miguelgonzalezcruz/web_project_4

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+    name: "Arches National Park",
+    link: "https://images.unsplash.com/photo-1605999212792-bc961b37bb55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
   },
   {
     name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+    link: "https://images.unsplash.com/photo-1625229250317-071640d8f9ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80",
   },
   {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+    name: "Olympic National Park",
+    link: "https://images.unsplash.com/photo-1639660451092-58a33eb251e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
   },
   {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+    name: "Glacier National Park",
+    link: "https://images.unsplash.com/photo-1517909568143-3eea286ca180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
   },
   {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+    name: "Yellowstone",
+    link: "https://images.unsplash.com/photo-1579824893947-6ec92314d5de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
   },
   {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    name: "Bryce Canyon",
+    link: "https://images.unsplash.com/photo-1651613628753-6c9690b72de2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1433&q=80",
   },
 ];
 
@@ -35,14 +35,20 @@ const editPopupElement = document.querySelector(".edit-popup");
 const createPopupElement = document.querySelector(".create-popup");
 const formSubmit = document.querySelector(".popup__content-form");
 const formCreate = document.querySelector(".popup__create-form");
+const previewImagePopup = document.querySelector(".js-preview-popup");
 
 // Buttons and other DOM elements
 const profileEditButton = document.querySelector(".profile__info-edit");
 const profileAddButton = document.querySelector(".profile__add");
 const editPopupCloseButton = document.querySelector(".edit__close-button");
 const createPopupCloseButton = document.querySelector(".create__close-button");
+const previewPopupCloseButton = document.querySelector(
+  ".preview__close-button"
+);
 const profileInfoTitle = document.querySelector(".profile__info-title");
 const profileInfoSubtitle = document.querySelector(".profile__info-subtitle");
+const previewImageElement = document.querySelector(".popup__preview_image");
+const previewImageCaption = document.querySelector(".popup__preview_caption");
 
 // Form data
 const inputValueTitle = formSubmit.querySelector(".popup__input_content_name");
@@ -107,9 +113,15 @@ createPopupCloseButton.addEventListener("click", () => {
   togglePopupElement(createPopupElement);
 });
 
+previewPopupCloseButton.addEventListener("click", () => {
+  togglePopupElement(previewImagePopup);
+});
+
 const getCardElement = (data) => {
   const cardElement = cardTemplate.cloneNode(true);
   const deleteButton = cardElement.querySelector(".element__delete-icon");
+
+  const imagePreview = cardElement.querySelector(".element__image");
 
   cardElement.querySelector(".element__image").src = data.link;
   cardElement.querySelector(".element__content-title").textContent = data.name;
@@ -121,6 +133,12 @@ const getCardElement = (data) => {
 
   deleteButton.addEventListener("click", () => {
     cardElement.remove();
+  });
+
+  imagePreview.addEventListener("click", function () {
+    previewImageElement.src = data.link;
+    previewImageCaption.textContent = data.name;
+    togglePopupElement(previewImagePopup);
   });
 
   return cardElement;
