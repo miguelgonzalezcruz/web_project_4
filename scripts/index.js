d105fdeb253cb2637252936ce28db822f6d0810d
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2022-05-08 20:26:28 +0200

    Project 5 v0

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,67 @@
-//Profile content
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
 
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".element");
+
+// Wrappers
+const placesWrap = document.querySelector(".elements");
+const editPopupElement = document.querySelector(".edit-popup");
+const createPopupElement = document.querySelector(".create-popup");
+const formSubmit = document.querySelector(".popup__content-form");
+const formCreate = document.querySelector(".popup__create-form");
+
+// Buttons and other DOM elements
+const profileEditButton = document.querySelector(".profile__info-edit");
+const profileAddButton = document.querySelector(".profile__add");
+const editPopupCloseButton = document.querySelector(".edit__close-button");
+const createPopupCloseButton = document.querySelector(".create__close-button");
 const profileInfoTitle = document.querySelector(".profile__info-title");
 const profileInfoSubtitle = document.querySelector(".profile__info-subtitle");
 
-//Open popup
-const popupElement = document.querySelector(".popup");
-const profileEditButton = document.querySelector(".profile__info-edit");
-const inputValueTitle = document.querySelector(".popup__input_content_name");
-const inputValueSubtitle = document.querySelector(".popup__input_content_role");
+// Form data
+const inputValueTitle = formSubmit.querySelector(".popup__input_content_name");
+const inputValueSubtitle = formSubmit.querySelector(
+  ".popup__input_content_role"
+);
 
-function openPopup() {
-  inputValueTitle.value = profileInfoTitle.textContent;
-  inputValueSubtitle.value = profileInfoSubtitle.textContent;
+const nameValue = formCreate.querySelector(".popup__input_content_name");
+const linkValue = formCreate.querySelector(".popup__input_content_link");
 
-  popupElement.classList.add("popup_is-opened");
+// Functions
+
+function togglePopupElement(modalWindow) {
+  if (!modalWindow.classList.contains("popup_is")) {
+    inputValueTitle.value = profileInfoTitle.textContent;
+    inputValueSubtitle.value = profileInfoSubtitle.textContent;
+  }
+  modalWindow.classList.toggle("popup_is-opened");
 }
-
-profileEditButton.addEventListener("click", openPopup);
-
-//End Open popup
-
-//Close popup
-
-const popupCloseButton = document.querySelector(".popup__content-close");
-
-function closePopup() {
-  popupElement.classList.remove("popup_is-opened");
-}
-
-popupCloseButton.addEventListener("click", closePopup);
-
-//End Close popup
-
-//Edit Profile form
-
-const formSubmit = document.querySelector(".popup__content-form");
 
 function editProfileContent(evt) {
   evt.preventDefault();
@@ -42,21 +69,68 @@ function editProfileContent(evt) {
   profileInfoTitle.textContent = inputValueTitle.value;
   profileInfoSubtitle.textContent = inputValueSubtitle.value;
 
-  closePopup();
+  togglePopupElement(editPopupElement);
 }
 
+function createContent(evt) {
+  evt.preventDefault();
+
+  renderCard(
+    {
+      name: nameValue.value,
+      link: linkValue.value,
+    },
+    placesWrap
+  );
+
+  togglePopupElement(createPopupElement);
+}
+
+// Event listerners
+
 formSubmit.addEventListener("submit", editProfileContent);
+formCreate.addEventListener("submit", createContent);
 
-// End Edit Profile Form
+profileEditButton.addEventListener("click", () => {
+  togglePopupElement(editPopupElement);
+});
 
-// Heart icon only works with the first card. No need to fix it for now
+profileAddButton.addEventListener("click", () => {
+  togglePopupElement(createPopupElement);
+});
 
-// const heartInactive = document.querySelector(".element__content-icon");
+editPopupCloseButton.addEventListener("click", () => {
+  togglePopupElement(editPopupElement);
+});
 
-// function activeHeart() {
-//   heartInactive.classList.toggle("active__heart");
-// }
+createPopupCloseButton.addEventListener("click", () => {
+  togglePopupElement(createPopupElement);
+});
 
-// heartInactive.addEventListener("click", activeHeart);
+const getCardElement = (data) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  const deleteButton = cardElement.querySelector(".element__delete-icon");
 
-// End Heart Icon
+  cardElement.querySelector(".element__image").src = data.link;
+  cardElement.querySelector(".element__content-title").textContent = data.name;
+  cardElement
+    .querySelector(".element__content-icon")
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("active__heart");
+    });
+
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  return cardElement;
+};
+
+const renderCard = (data, wrapper) => {
+  const newCard = getCardElement(data);
+  wrapper.prepend(newCard);
+};
+
+initialCards.forEach((data) => {
+  renderCard(data, placesWrap);
+});
