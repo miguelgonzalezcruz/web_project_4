6d598c6964eceecc80bdd0a6f1f1e29f41043951
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2022-08-14 22:14:39 +0200

    Cards and info data from server

--- src/components/test.js
+++ src/components/test.js
@@ -3,3 +3,26 @@
 // Group ID: group-12
 
 // EDITA EL NOMBRE DE USUARIO
+
+const user = new UserInfo({
+  userName: ".profile__info-name",
+  userJob: ".profile__info-title",
+  userAvatar: ".profile__avatar",
+});
+
+let UserId = null;
+
+Promise.all([api.getUserInfo(), api.getInitialCards()])
+  .then(([userInfoResponse, cardsResponse]) => {
+    //user info set up
+    user.setAvatar(userInfoResponse.avatar);
+    UserId = userInfoResponse._id;
+    user.setUserInfoTextOnly(userInfoResponse);
+    //cards set up
+    cardGridObject.setItems(cardsResponse);
+
+    cardGridObject.renderItems();
+  })
+  .catch((err) => {
+    console.log(err); // log the error to the console
+  });
